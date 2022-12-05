d87826d38203294a944c252d6603b83b0f70725d
Johann7777 <johannhooi7@gmail.com>
2022-12-05 13:51:31 +0800

    Revert "Update streamlit_app.py"
    This reverts commit 5f39b203ecc96d6c88c4c78ac053c99cc400c836.

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,7 +113,7 @@ def main():
 
 def load_model():
     model_ft, input_size = initialize_model("squeezenet", 10, True)
-    checkpoint = torch.load(modeldir, map_location=torch.device('cpu'))
+    checkpoint = torch.load(modeldir)
     model_ft.load_state_dict(checkpoint['model_state_dict'])
     model_ft.eval()
     return model_ft
